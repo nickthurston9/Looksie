3c37c64d7d5b9512582b81aaacb7055e7e0423e1
Nick Thurston <32639858+nickthurston9@users.noreply.github.com>
2021-09-28 21:17:45 -0400

    Add files via upload
    Initial upload of looksie.py

---
+++ looksie.py
@@ -0,0 +1,42 @@
+import webbrowser
+from tkinter import * 
+import tkinter.messagebox 
+
+
+print("")
+print("██╗      ██████╗  ██████╗ ██╗  ██╗███████╗██╗███████╗")
+print("██║     ██╔═══██╗██╔═══██╗██║ ██╔╝██╔════╝██║██╔════╝")
+print("██║     ██║   ██║██║   ██║█████╔╝ ███████╗██║█████╗  ")
+print("██║     ██║   ██║██║   ██║██╔═██╗ ╚════██║██║██╔══╝  ")
+print("███████╗╚██████╔╝╚██████╔╝██║  ██╗███████║██║███████╗")
+print("╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝╚══════╝")
+print("")
+
+# Ask for what to search for
+searchItem = input("What are we searching for today?" + "\n\n")
+
+print("\n\n")
+
+print("#VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV#")
+print("#>>>>>> Start the search queries!!! <<<<<<#")
+print("###########################################")
+
+# Search Google
+webbrowser.open_new_tab(f"https://google.com/search?q={searchItem}")
+# Search DuckDuckGo
+webbrowser.open_new_tab(f"https://duckduckgo.com/?q={searchItem}")
+# Search Yahoo
+webbrowser.open_new_tab(f"https://search.yahoo.com/search?p={searchItem}")
+# Search YouTube
+webbrowser.open_new_tab(f"https://youtube.com/results?search_query={searchItem}")
+# Search Facebook
+webbrowser.open_new_tab(f"https://facebook.com/search/top/?q={searchItem}")
+# Search LinkedIn
+webbrowser.open_new_tab(f"https://linkedin.com/search/results/all/?keywords={searchItem}")
+
+### Display message to tell user that the results are complete
+
+# Close root tkinter window
+Tk().withdraw()
+# Display messagebox with info about results
+result=tkinter.messagebox.showinfo('Looksie','Results are shown in your web browser in the tabs.')
