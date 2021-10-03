5624501aff285c8e5c083278adc5f291417b2ed0
Nick Thurston <32639858+nickthurston9@users.noreply.github.com>
2021-10-02 23:02:16 -0400

    Shodan Search Added
    Added ability to search Shodan.

--- looksie.py
+++ looksie.py
@@ -36,6 +36,24 @@ webbrowser.open_new_tab(f"https://facebook.com/search/top/?q={searchItem}")
 # Search LinkedIn
 webbrowser.open_new_tab(f"https://linkedin.com/search/results/all/?keywords={searchItem}")
 
+
+
+### Ask if user would like to search shodan
+
+shodanResponse = input("\n\n Would you like to search shodan.io? (y/n)")
+
+if shodanResponse == "Y" or shodanResponse == "y":
+    print("Searching shodan.io... \n")
+    webbrowser.open_new_tab(f"https://www.shodan.io/search?query={searchItem}")
+    exit()
+elif shodanResponse == "N" or shodanResponse == "n":
+    print("Not searching shodan.io")
+    exit()
+else:
+    print("Not a vaild option. Exiting.")
+    exit()
+
+
 ### Display message to tell user that the results are complete
 
 # Close root tkinter window
